233ec2b0b549675cab0a3d80d27b9ade7cb0fbe8
BIH <bih198721@gmail.com>
2021-03-25 02:05:15 +0900

    get location provider

--- app/src/main/java/com/example/gps_test/MainActivity.java
+++ app/src/main/java/com/example/gps_test/MainActivity.java
@@ -8,232 +8,136 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
+import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private Context context = MainActivity.this;
+    private final Context context = MainActivity.this;
+    private final Activity activity = this;
     private final static String TAG = "Debugging";
 
-    private String isGPSEnabled = null;
-    private int isGPSPermitted = -1;
+//    private String isGPSEnabled = null;
+//    private int isGPSPermitted = -1;
     private final static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private final static int ACT_SETTING = 0;
+    private final static int ACT_PERMISSION = 0;
+    private final static int ACT_LOCATION = 1;
     private boolean isRepeated;
-    //    private Snackbar snackbar;
-    private View mainView;
+    private final MLocationListner mLocationListner = new MLocationListner();
+    private LocationManager locationManager;
 
-    private double lat, lon;
+    private TextView textView;
 
+    private double curLat, curLon;
+
+    private int cnt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        mainView = findViewById(R.id.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
 
-//        // 권한 요청
-//        // ~을 허용하시겠습니까?
-//        ActivityCompat.requestPermissions(this,
-//                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-//                MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-        getGPSPermission();
-//        setGPSEnabled();
-
-//        clickHandler(mainView);
-
-        MLocationListner mLocationListner = new MLocationListner();
-        LocationManager locationManager = (LocationManager)
+        locationManager = (LocationManager)
                 this.getSystemService(Context.LOCATION_SERVICE);
 
-        List<String> providers = locationManager.getAllProviders();
+        getGPSPermission();
 
-        String str = "";
-        for (int i=0; i<providers.size(); i++) {
-            str += "Provider "+providers.get(i) + " : "+
-                    locationManager.isProviderEnabled(providers.get(i))+"\n";
-        }
-        textView.setText(str);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                3000, 0, mLocationListner);
     }
-
-    /*
-    // 스낵바 팝업 생성
-    public void clickHandler(View v) {
-        Snackbar snackbar = Snackbar.make(v, "Snackbar Test", Snackbar.LENGTH_INDEFINITE);
-
-        snackbar.setAction("OK", new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                snackbar.dismiss();
-            }
-        });
-
-        snackbar.show();
-    }
-
-     */
-
-
-/*
-    private void getGPSPermission() {
-        Log.d("Debug", ""+shouldShowRequestPermissionRationale);
-        Log.d("Debug", ""+isGPSPermitted);
-        isGPSPermitted = ActivityCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_FINE_LOCATION);
-        String str = null;
-        switch (isGPSPermitted) {
-            case PackageManager.PERMISSION_DENIED:
-                str = "PERMISSION_DENIED";
-                break;
-            case PackageManager.PERMISSION_GRANTED:
-                str = "PERMISSION_GRANTED";
-                break;
-        }
-//        Toast.makeText(context, "isGPSPermitted : "+str, Toast.LENGTH_SHORT).show();
-        Log.d("Debug", str+" "+isGPSPermitted);
-        if (isGPSPermitted != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(context, "권한 승인이 필요합니다.", Toast.LENGTH_SHORT).show();
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // 다시 묻지 않음 체크 안함
-                Log.d("Debug", "다시 묻지 않음 체크 안함");
-
-                ActivityCompat.requestPermissions(this,
-                        new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-                AlertDialog.Builder dlg = new AlertDialog.Builder(context);
-                dlg.setMessage("위치권한 설정 필요 이유");
-                dlg.setTitle("Permission");
-                dlg.setIcon(R.drawable.ic_launcher_background);
-                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-                dlg.show();
-            } else {
-                // 최초 실행
-                // 다시 묻지 않음 체크
-                Log.d("Debug", "다시 묻지 않음 체크");
-                Toast.makeText(context, "권한 승인 필요", Toast.LENGTH_SHORT).show();
-                ActivityCompat.requestPermissions(this,
-                        new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-                AlertDialog.Builder dlg = new AlertDialog.Builder(context);
-
-                dlg.setMessage("위치권한 설정 필요 이유");
-                dlg.setTitle("다시 묻지 않음");
-                dlg.setIcon(R.drawable.ic_launcher_background);
-                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-                dlg.show();
-            }
-        }
-        Log.d("Debug", ""+isGPSPermitted);
-    }
-
- */
 
     /* GPS 권한 */
+    // 0. Check GPS Permission
+    private boolean checkGPSPermission() {
+        int permission = ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permission == PackageManager.PERMISSION_GRANTED;
+    }
+
     // 1. GPS 권한 확인
     private void getGPSPermission() {
-        isGPSPermitted = ActivityCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_FINE_LOCATION);
+//        isGPSPermitted = ActivityCompat.checkSelfPermission(context,
+//                Manifest.permission.ACCESS_FINE_LOCATION);
 
-        if (isGPSPermitted != PackageManager.PERMISSION_GRANTED) {
+        if (!checkGPSPermission()) {
             // 권한 설정 안되어 있을 경우
             // 권한 설정
             ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            setGPSEnabled();
         }
     }
 
     // 2. 권한 요청 다이얼로그 결과에 대한 액션
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION:
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Accept
-                    Log.d("Debug", "Permission accepted");
+        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Accept
+                Log.d("Debug", "Permission accepted");
+                setGPSEnabled();
+            } else {
+                // Deny
+                Log.d(TAG, "Permission denied");
+                isRepeated = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+                if (isRepeated) {
+                    // 다시 묻지 않음 체크 안함
+                    alertPermissionGPS(isRepeated,
+                            "GPS 사용 권한이 설정되어있지 않아\n" +
+                                    "앱 사용이 불가능합니다.\n" +
+                                    "GPS 사용 권한을 설정하시겠습니까?");
                 } else {
-                    // Deny
-                    Log.d(TAG, "Permission denied");
-                    isRepeated = ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.ACCESS_FINE_LOCATION);
-                    if (isRepeated) {
-                        // 다시 묻지 않음 체크 안함
-                        alertPermissionGPS(isRepeated, "권한 설정 필요", "사용불가");
-                    } else {
-                        // 다시 묻지 않음 체크
-                        alertPermissionGPS(isRepeated, "권한 설정 필요",
-                                "사용불가\n설정창 이동?");
-                    }
+                    // 다시 묻지 않음 체크
+                    alertPermissionGPS(isRepeated,
+                            "GPS 사용 권한이 설정되어있지 않아\n" +
+                                    "앱 사용이 불가능합니다.\n" +
+                                    "GPS 사용 권한을 설정하시겠습니까?");
                 }
+            }
         }
     }
 
     // 3. GPS 권한 거부 이벤트
-    private void alertPermissionGPS(boolean repeated, String title, String msg) {
+    private void alertPermissionGPS(boolean repeated, String msg) {
         AlertDialog.Builder dlg = new AlertDialog.Builder(context);
-        dlg.setTitle(title)
+        dlg.setTitle("GPS 사용 권한")
                 .setMessage(msg);
         if (repeated) {
-            dlg.setPositiveButton("CANCEL",
+            dlg.setPositiveButton("ACCEPT",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(activity,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                        }
+                    })
+                    .setNegativeButton("DENY",
                     new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
                             dialogInterface.cancel();
+                            alertFinish();
                         }
-                    })
-                    .show();
+                    }).show();
         } else {
             dlg.setPositiveButton("ACCEPT",
                     new DialogInterface.OnClickListener() {
@@ -247,61 +151,85 @@ public class MainActivity extends AppCompatActivity {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
                                     dialogInterface.cancel();
+                                    alertFinish();
                                 }
                             })
                     .show();
         }
     }
 
-//    Handler mHandler = new Handler(Looper.getMainLooper()) {
-//        @Override
-//        public void handleMessage(@NonNull Message msg) {
-//            if (msg.what == 0) {
-//                setGPSEnabled();
-//            }
-//        }
-//    };
+    // #. 앱 종료 다이얼로그
+    private void alertFinish() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("앱 사용 불가")
+                .setMessage("앱을 종료합니다.")
+                .setPositiveButton("FINISH",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                finish();
+                            }
+                        }).show();
+    }
 
     // 4. GPS 권한 설정 화면
     private void setGPSPermission() {
         Intent GPSPmsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
         Uri uri = Uri.fromParts("package", getPackageName(), null);
         GPSPmsIntent.setData(uri);
-        startActivityForResult(GPSPmsIntent, ACT_SETTING);
+        startActivityForResult(GPSPmsIntent, ACT_PERMISSION);
     }
-
-
 
     // 5. GPS 권한 설정 종료 이벤트
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
-            case ACT_SETTING:
+            case ACT_PERMISSION:
                 Log.d(TAG, "Setting activity finished");
 
-                isGPSPermitted = ActivityCompat.checkSelfPermission(context,
-                        Manifest.permission.ACCESS_FINE_LOCATION);
+//                isGPSPermitted = ActivityCompat.checkSelfPermission(context,
+//                        Manifest.permission.ACCESS_FINE_LOCATION);
 
-                if (isGPSPermitted == PackageManager.PERMISSION_GRANTED) {
+                if (checkGPSPermission()) {
                     setGPSEnabled();
                     break;
+                } else {
+                    alertPermissionGPS(isRepeated,
+                            "GPS 사용 권한이 설정되어있지 않아\n" +
+                                    "앱 사용이 불가능합니다.\n" +
+                                    "GPS 사용 권한을 설정하시겠습니까?");
+                }
+            case ACT_LOCATION:
+                if (isGPSEnabled()) {
+                    getLocation();
+                } else {
+                    setGPSEnabled();
                 }
         }
     }
 
     /* GPS 활성화 */
-    // 1. GPS 활성 상태 확인
-    private void setGPSEnabled() {
-        isGPSEnabled = Settings.Secure.getString(getContentResolver(),
+    // 0. GPS 활성 상태 확인
+    private boolean isGPSEnabled() {
+        String activated = Settings.Secure.getString(getContentResolver(),
                 Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
-        Log.d("Debug", "enabled : "+isGPSEnabled);
+        return activated.contains("gps") && activated.contains("network");
+    }
 
-        if (!(isGPSEnabled.contains("gps")&&isGPSEnabled.contains("network"))) {
+    // 1. GPS 활성화
+    private void setGPSEnabled() {
+//        isGPSEnabled = Settings.Secure.getString(getContentResolver(),
+//                Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+//        Log.d("Debug", "enabled : " + isGPSEnabled);
+
+        if (isGPSEnabled()) {
+            Log.d(TAG, "GPS is enabled");
+            getLocation();
+        } else {
             Log.d(TAG, "GPS is not enabled");
             alertCheckGPS();
-        } else {
-            Log.d(TAG, "GPS is enabled");
         }
     }
 
@@ -321,6 +249,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         dialogInterface.cancel();
+                        alertFinish();
                     }
                 })
                 .show();
@@ -330,22 +259,63 @@ public class MainActivity extends AppCompatActivity {
     private void setGPSConfig() {
         Intent GPSOptIntent = new Intent(
                 Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-        startActivity(GPSOptIntent);
+        startActivityForResult(GPSOptIntent, ACT_LOCATION);
     }
 
     /* LocationProvider */
+
+    // Get Best Provider
+    private String getBestProvider() {
+        locationManager = (LocationManager)
+                this.getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria crit = new Criteria();
+        crit.setAccuracy(Criteria.NO_REQUIREMENT);
+        crit.setPowerRequirement(Criteria.NO_REQUIREMENT);
+        crit.setAltitudeRequired(false);
+        crit.setCostAllowed(false);
+        return locationManager.getBestProvider(crit, true);
+    }
+
+    // Location Listener
     public class MLocationListner implements LocationListener {
         @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) { }
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d("TAG", getBestProvider());
+        }
+
         @Override
-        public void onProviderEnabled(@NonNull String provider) { }
+        public void onProviderEnabled(@NonNull String provider) {
+            Log.d("TAG", provider+" enabled");
+        }
+
         @Override
-        public void onProviderDisabled(@NonNull String provider) { }
+        public void onProviderDisabled(@NonNull String provider) {
+            Log.d("TAG", provider+" disabled");
+        }
+
         @Override
         public void onLocationChanged(@NonNull Location location) {
-            lat = location.getLatitude();
-            lon = location.getLongitude();
-            Log.d(TAG, lat+","+lon);
+            cnt++;
+            curLat = location.getLatitude();
+            curLon = location.getLongitude();
+            Log.d(TAG, "**"+cnt+"**"+curLat + "," + curLon);
+            textView.setText(curLat + "," + curLon);
+        }
+    }
+
+    private void getLocation() {
+//        isGPSPermitted = checkGPSPermission();
+//        if (isGPSPermitted != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+        if (checkGPSPermission()) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 0, mLocationListner);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    300, 0, mLocationListner);
+            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER,
+                    1000, 0, mLocationListner);
         }
     }
 }
